514d179944e5e869df219e9620382cfaf55b6435
kelosn <114831705+kelosn@users.noreply.github.com>
2023-04-16 07:57:11 -0700

    Add files via upload

---
+++ project/Program.cs
@@ -0,0 +1,28 @@
+﻿ using System;
+
+namespace QuadraticEquationSolver
+{
+    public class QuadraticEquation
+    {
+        public static (double?, double?) Solve(double a, double b, double c)
+        {
+            double delta = b * b - 4 * a * c;
+
+            if (delta < 0)
+            {
+                return (null, null);
+            }
+            else if (delta == 0)
+            {
+                double root = -b / (2 * a);
+                return (root, null);
+            }
+            else
+            {
+                double root1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double root2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                return (root1, root2);
+            }
+        }
+    }
+}
